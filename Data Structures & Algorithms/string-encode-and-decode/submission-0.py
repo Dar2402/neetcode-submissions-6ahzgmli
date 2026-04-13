dbb70afc43978b9046f26df16fac328f6e45cd5c
Darshan Pandey <71745841+Dar2402@users.noreply.github.com>
2026-04-13 18:28:21 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def transform(self, s, n, to_do="en"):
+        temp = ""
+        if to_do == "en":
+            temp = str(n) + "#"
+            
+            for i in s:
+                temp = temp + chr(ord(i) + n)
+            
+            return temp
+        else:
+            for i in s:
+                temp = temp + chr(ord(i) - n)
+            return temp
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for i in strs:
+            n = len(i)
+            res = res + self.transform(i, n)
+
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res2 = []
+        for i in range(len(s)):
+            if s[i].isdigit():
+                res2.append(self.transform(s[i + 2: i + int(s[i]) + 2], int(s[i]), "de"))
+        return res2
